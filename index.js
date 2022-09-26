8f292c1e314dd43914e41ab31ffdc8aa6bc2da4f
Mahmud2640 <mdmahmud484@gmail.com>
2022-09-26 19:09:30 +0600

    mongoDB Connect

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
 
-app.listen(8800, ()=>{
-   console.log("Connected to backend server")
-})
+mongoose.connection.on("disconnect", () => {
+  console.log("MongoDB disconnect");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend server");
+});

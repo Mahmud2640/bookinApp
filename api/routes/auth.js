7c28a0bfe60a5a64c511a041334ad1f6f29a2d7d
Mahmud2640 <mdmahmud484@gmail.com>
2022-09-27 16:57:07 +0600

    api get,put

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,8 +3,10 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello From auth server");
+  res.send("Hello From auth endpoint");
 });
-
+router.get("/register", (req, res) => {
+  res.send("Hello From auth Register endpoint");
+});
 
 export default router;

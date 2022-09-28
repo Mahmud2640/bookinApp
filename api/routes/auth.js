846a475702f5ee75e729db4fc5e225a876057fb9
Mahmud2640 <mdmahmud484@gmail.com>
2022-09-28 16:03:09 +0600

    user added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,8 @@
 import express from "express";
+import { register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello From auth endpoint");
-});
-router.get("/register", (req, res) => {
-  res.send("Hello From auth Register endpoint");
-});
+router.post("/register", register);
 
 export default router;
